0b2224ca4133a44e43d118cd96baa50317a8c4e7
Laetus-ustc <jiujhsl@gmail.com>
2022-08-19 14:25:14 +0800

    lab1Finished?

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,13 @@ found:
     return 0;
   }
 
+  if((p->pidpage = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -140,6 +147,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p->pidpage->pid = p->pid;
 
   return p;
 }
@@ -150,6 +158,8 @@ found:
 static void
 freeproc(struct proc *p)
 {
+  if(p->pidpage)
+    kfree((void*)p->pidpage);
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
@@ -196,6 +206,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  if (mappages(pagetable, USYSCALL, PGSIZE, 
+               (uint64)(p->pidpage), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +224,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
